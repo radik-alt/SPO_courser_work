7ef8b659f0477a6cde89b46b59fa95aa23809930
Radik <Radik.app@yandex.ru>
2023-06-04 17:16:57 +0400

    test start

--- git_branch/tests.py
+++ git_branch/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APITestCase
+
 
 # Create your tests here.
+class TestApiLevels(APITestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_levels(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/customer/details')
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = my_view(request)
+        self.assertEqual(response.status_code, 200)
+    def test_tasks(self):
+        response = self.client.get("http://127.0.0.1:8000/api/v1/tasks")
+        self.assertEqual(response.status_code, 200)
+
+
--- git_branch/urls.py
+++ git_branch/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from git_branch.views import *
 
 urlpatterns = [
-    path('api/v1/levels', LevelsApi.as_view()),
-    path('api/v1/tasks', TaskApi.as_view()),
-    path('api/v1/tasks/<int:level>/', TaskFromLevel.as_view()),
-    path('api/v1/task_info/<int:task_id>/', TaskInfoApiView.as_view()),
-    path('api/v1/git_info', GitInfoApiView.as_view())
+    path('levels', LevelsApi.as_view()),
+    path('tasks', TaskApi.as_view()),
+    path('tasks/<int:level>/', TaskFromLevel.as_view()),
+    path('task_info/<int:task_id>/', TaskInfoApiView.as_view()),
+    path('git_info', GitInfoApiView.as_view())
 ]
